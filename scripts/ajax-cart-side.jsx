03a12fc5cd9eb99d4e98a39f29182c1faafc7a69
Roman <roman.gavrilov.0309@gmail.com>
2021-10-13 15:42:57 -0400

    Inital Setup

--- scripts/ajax-cart-side.jsx
+++ scripts/ajax-cart-side.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDom from "react-dom"
 
 const Test = () => {
-	return <div>This is tutorial to integrate react.js into shopify theme with webpack</div>
+	return <div>This is tutorial to integrate react.js into shopify theme with webpack!!!</div>
 }
 
 const root = document.getElementById('cart__drawer_items');
